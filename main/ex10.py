f89058ceb7d08f632694aaa40e360129f40a578b
GagauzS <gaga-ya@hotmail.com>
2015-07-07 21:27:02 +0300

    Added lesson#10

---
+++ main/ex10.py
@@ -0,0 +1,19 @@
+__author__ = 'Gagauz2010'
+# step 10
+# what was that?
+
+tabby_cat = '\tI\'m tabbed in.'
+persian_cat = 'I\'m split\non line.'
+backslash_cat = 'I\'m \\ a \\ cat.'
+
+fat_cat="""
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
