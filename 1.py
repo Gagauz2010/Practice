d0e2ab77659145bdba6f38dbc7327d146678ede3
GagauzS <gaga-ya@hotmail.com>
2015-07-07 18:23:09 +0300

    Added lesson#1

---
+++ 1.py
@@ -0,0 +1,11 @@
+__author__ = 'Gagauz2010'
+# first step
+# print command
+
+print("Hello world")
+print("Hello again")
+print("I like typing this")
+print("This is fun")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
