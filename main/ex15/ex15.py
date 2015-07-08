e0fba2dccb6b3677c723f5693f2aef2e5bb8e31a
GagauzS <gaga-ya@hotmail.com>
2015-07-08 12:25:40 +0300

    Added lesson#15

---
+++ main/ex15/ex15.py
@@ -0,0 +1,19 @@
+__author__ = 'Gagauz2010'
+# step 15
+# reading files
+
+from sys import argv
+
+script, filename = argv
+
+txt = open(filename)
+
+print('Here\'s your file %r:' % filename)
+print(txt.read())
+
+print('Type the filename again:')
+filename_again = input('> ')
+
+txt_again = open(filename_again)
+
+print(txt_again.read())
